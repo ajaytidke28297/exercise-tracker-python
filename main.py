01b241d26ad965b9dd13e7b786bcd6857a628d78
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-12-12 23:46:57 +0530

    Complete exercise tracker and update the details in google sheet using sheety API

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import requests
 from dotenv import load_dotenv
 import os
+import requests
+from datetime import datetime
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -19,16 +20,47 @@ nutix_headers = {
     "x-app-key": os.getenv("NUTRITIONIX_API_KEY"),
 }
 
-exercise = input("What exercise did you do?: ")
-print(f"Query: {exercise}")
+exercise_text = input("What exercise did you do?: ")
+print(f"Query: {exercise_text}")
 
 nutix_params = {
-    "query": exercise,
+    "query": exercise_text,
     "gender": GENDER,
     "weight_kg": WEIGHT_KG,
     "height_cm": HEIGHT_CM,
     "age": AGE
 }
 
-response = requests.post(url=NUTRITIONIX_END_POINT, json=nutix_params, headers=nutix_headers)
-print(f"Response text: {response.text}")
+nutix_response = requests.post(url=NUTRITIONIX_END_POINT, json=nutix_params, headers=nutix_headers)
+nutix_response.raise_for_status()
+data = nutix_response.json()['exercises']
+print(f"Response text: {nutix_response.text}")
+
+# Current date and time in dd/mm/yyyy and hh:mm:ss resp.
+now = datetime.now()
+today_date = now.strftime("%d/%m/%Y")
+today_time = now.strftime("%H:%M:%S")
+
+# Sheety API
+
+SHEETY_END_POINT = "https://api.sheety.co/90203dcb0b578ada8862d86b9cefcc1e/workoutTracking/workouts"
+
+bearer_headers = {
+    "Authorization": f"Bearer {os.getenv('SHEETY_TOKEN')}"
+}
+
+print(f"bearer token: {bearer_headers}")
+
+for exercise in data:
+    workout = {
+        "workout": {
+            "date": today_date,
+            "time": today_time,
+            "exercise": exercise['name'].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheety_response = requests.post(url=SHEETY_END_POINT, json=workout, headers=bearer_headers)
+    print(f"Response: {sheety_response.json()}")
